c1cc17ea34b019dddb6a4994ba3ddf84d6ff22d8
ffore <daniel.geleroff@gmail.com>
2019-01-15 14:19:56 -0500

    finished

--- oneaway.java
+++ oneaway.java
@@ -1,8 +1,9 @@
 import java.util.Objects;
+import java.util.Scanner;
 
 class oneaway{
 	public static boolean checkRepl(String a, String b){
-		if(Objects.equals(a, b)) return false;
+		if(Objects.equals(a, b)) return true;
 
 		for(int i = 0; i < a.length(); i++){
 			if(a.charAt(i) != b.charAt(i)){
@@ -23,8 +24,66 @@ class oneaway{
 		return false;
 	}
 
+	public static boolean checkRemo(String a, String b){
+
+		String l0ng;
+		String sh0rt;
+		if(a.length() > b.length()){
+			l0ng = a;
+			sh0rt = b;
+		}
+		else{
+			l0ng = b;
+			sh0rt = a;
+		}
+		
+		//if l0ng is 1 or 2 chars, always return true
+		if(l0ng.length() == 1 || l0ng.length() == 2){
+			return true;
+		}
+		int size = l0ng.length();
+		for(int i = 0; i < size; i++){
+			if(i == sh0rt.length()-1 && l0ng.charAt(i) == sh0rt.charAt(i)){
+				//example	long:	abcd
+				//			short:	abc
+				return true;
+			}
+			if(l0ng.charAt(i) != sh0rt.charAt(i)){
+				StringBuilder sb = new StringBuilder(l0ng);
+				sb.deleteCharAt(i);
+				l0ng = sb.toString();
+				if(Objects.equals(l0ng, sh0rt)) return true;
+				else return false;
+			}
+		}
+		return false;
+	}
+
 	public static void main(String[] args){
-		System.out.println(checkRepl("daniel","dadiel"));
+		String a;
+		String b;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("enter string 1: ");
+		a = sc.nextLine();
+		System.out.println("");
+		System.out.print("enter string 2: ");
+		b = sc.nextLine();
+		System.out.println("");
+		
+		if(a.length() -b.length() == -1 || a.length() -b.length() == 1){
+			System.out.println(checkRemo(a, b));
+			return;
+		}
+		if(a.length() == b.length()){
+			System.out.println(checkRepl(a, b));
+			return;
+		}
+		else{
+			System.out.println("false");
+			return;
+		}
 	}
 }
 
